9f0a4e5e094d8cc5b8e334015b304362a066c823
kathmarioly <katerinemarioly@gmail.com>
2017-05-05 01:08:31 -0300

    Miembros de mi Squad

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+function Persona(nombre, apellido, edad, hobbies){
+
+	this.nombre = nombre;
+	this.apellido = apellido;
+	this.edad = edad;
+	this.hobbies = hobbies;
+	this.saludar = function(){
+		document.write(" <strong>Nombre: </strong> " + this.nombre + " <strong>Apellido:</strong> " + this.apellido + " <strong>Edad:</strong> " + this.edad + " <strong>Hobbiets: </strong> " + this.hobbies )
+	}
+
+
+}
+var Romina = new Persona("Romina", "Torres", 31, "Componer, Cantar, Tocar instrumentos.")
+Romina.saludar();
+var Paulina = new Persona("Paulina", "Gonzales", 26, "Video-juegos, Series, Manualidades.")
+Paulina.saludar();
+var Katerine = new Persona("Katerine", "Sandoval", 24, "Arte, Musica, Literatura.")
+Katerine.saludar();
+var Natalia = new Persona("Natalia", "Garrido", 27, "Animales, Comida, Series.")
+Natalia.saludar();
+var Natalia = new Persona("Natalia", "Vivanco", 27, "Leer, Jardinear, Manualidades.")
+Natalia.saludar();
+
